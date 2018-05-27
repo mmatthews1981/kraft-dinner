b258b9ea32d2feddf174b3b17812fabc829e7ac6
mmatthews1981 <meredith.lynn.matthews@gmail.com>
2018-05-27 16:40:59 -0400

    refactored all statemachine objects into factory functions mostly to get them out of the way.

---
+++ game.js
@@ -0,0 +1,371 @@
+function home() {
+    'use strict';
+    return {
+        description: 'You\'re feeling hungry. <span class="js-look">Look around the kitchen.</span>'
+    };
+}
+
+function kitchen() {
+    'use strict';
+    return {
+        description: 'You look around the kitchen. Your fridge is in the corner, humming. The stove is to your left. The cabinets are directly in front of you. The sink is to your right. Conveniently, your favorite dinner bowl is sitting next to the sink.'
+    };
+}
+
+function pot() {
+    'use strict';
+    return new StateMachine({
+        init: 'cabinet',
+        data: {
+            type: 'vessel',
+            name: 'spaghetti pot'
+        },
+        transitions: [
+            {name: 'pickUp', from: ['cabinet', 'stove', 'sink'], to: 'inventory'},
+            {name: 'potOnStove', from: 'inventory', to: 'stove'},
+            {name: 'potInSink', from: 'inventory', to: 'sink'}
+        ],
+        methods: {
+            onPickUp: function () {
+                appendWindow('You pick up the pot.');
+            },
+            onPotOnStove: function () {
+                appendWindow('You put the pot on the stove.');
+            },
+            onPotInSink: function () {
+                appendWindow('You put the pot in the sink.');
+            }
+        }
+    });
+}
+
+function colander() {
+    'use strict';
+    return new StateMachine({
+        init: 'cabinet',
+        data: {
+            type: 'vessel',
+            name: 'colander'
+        },
+        transitions: [
+            {name: 'pickUp', from: ['cabinet', 'stove', 'sink'], to: 'inventory'},
+            {name: 'colanderOnStove', from: 'inventory', to: 'stove'},
+            {name: 'colanderInSink', from: 'inventory', to: 'sink'}
+        ],
+        methods: {
+            onPickUp: function () {
+                appendWindow('You pick up the colander.');
+            },
+            onColanderOnStove: function () {
+                appendWindow('You put the colander on the stove.');
+            },
+            onColanderInSink: function () {
+                appendWindow('You put the colander in the sink.');
+            }
+        }
+    });
+}
+
+function sink() {
+    'use strict';
+    return new StateMachine({
+        init: 'off',
+        data: {
+            type: 'location',
+            name: 'sink',
+            description: 'A typical steel apartment kitchen sink.'
+        },
+        transitions: [
+            {name: 'step', from: 'off', to: 'on'},
+            {name: 'step', from: 'on', to: 'off'}
+        ],
+        methods: {
+            onOn: function () {
+                appendWindow('You turn on the sink. The water is running.');
+            },
+            onOff: function () {
+                appendWindow('You turn off the stove. The water stops running.');
+            }
+        }
+    });
+}
+
+function stove() {
+    'use strict';
+    return new StateMachine({
+        init: 'off',
+        data: {
+            type: 'location',
+            name: 'stove',
+            description: 'A black electric stove. You should probably clean it.'
+        },
+        transitions: [
+            {name: 'step', from: 'off', to: 'on'},
+            {name: 'step', from: 'on', to: 'off'}
+        ],
+        methods: {
+            onOn: function () {
+                appendWindow('You turn on the stove. The element starts to glow.');
+            },
+            onOff: function () {
+                appendWindow('You turn off the stove. The element stops glowing.');
+            }
+        }
+    });
+}
+
+function cabinet() {
+    'use strict';
+    return new StateMachine({
+        init: 'closed',
+        data: {
+            type: 'location',
+            name: 'cabinet',
+            description: 'This kitchen does have a lot of cabinets, but you have a sneaking suspicion they\'re not attached to the wall.'
+        },
+        transitions: [
+            {name: 'step', from: 'closed', to: 'open'},
+            {name: 'step', from: 'open', to: 'closed'}
+        ],
+        methods: {
+            onOpen: function () {
+                appendWindow('You opened the cabinet.');
+            },
+            onClosed: function () {
+                appendWindow('You closed the cabinet.');
+            }
+        }
+    });
+}
+
+function fridge() {
+    'use strict';
+    return new StateMachine({
+        init: 'closed',
+        data: {
+            type: 'location',
+            name: 'fridge',
+            description: 'The top-freezer fridge that came with the apartment. Sometimes, if freezes your milk for no reason.'
+        },
+        transitions: [
+            {name: 'step', from: 'closed', to: 'open'},
+            {name: 'step', from: 'open', to: 'closed'}
+        ],
+        methods: {
+            onOpen: function () {
+                appendWindow('You opened the fridge.');
+            },
+            onClosed: function () {
+                appendWindow('You closed the fridge.');
+            }
+        }
+    });
+}
+
+function freezer() {
+    'use strict';
+    return new StateMachine({
+        init: 'closed',
+        data: {
+            type: 'location',
+            name: 'freezer',
+            description: 'The freezer. Mostly frozen veggies and geriatric bananas.'
+        },
+        transitions: [
+            {name: 'step', from: 'closed', to: 'open'},
+            {name: 'step', from: 'open', to: 'closed'}
+        ],
+        methods: {
+            onOpen: function () {
+                appendWindow('You opened the freezer.');
+            },
+            onClosed: function () {
+                appendWindow('You closed the freezer.');
+            }
+        }
+    });
+}
+
+function carrotsAndPeas() {
+    'use strict';
+    return new StateMachine({
+        init: 'freezer',
+        data: {
+            type: 'ingredient',
+            name: 'carrots and peas'
+        },
+        transitions: [
+            {name: 'pickUp', from: 'freezer', to: 'inventory'},
+            {name: 'addToPot', from: 'inventory', to: 'potContents'}
+        ],
+        methods: {
+            onPickUp: function () {
+                appendWindow('You take the bag from the freezer.');
+            },
+            onAddToPot: function () {
+                appendWindow('You empty the bag of peas and carrots into the pot.');
+            }
+        }
+    });
+}
+
+function butter() {
+    'use strict';
+    return new StateMachine({
+        init: 'refrigerator',
+        data: {
+            type: 'ingredient',
+            name: 'butter'
+        },
+        transitions: [
+            {name: 'pickUp', from: 'refrigerator', to: 'inventory'},
+            {name: 'addToPot', from: 'inventory', to: 'potContents'}
+        ],
+        methods: {
+            onPickUp: function () {
+                appendWindow('You take the butter from the refrigerator.');
+            },
+            onAddToPot: function () {
+                appendWindow('You add the butter to the pot.');
+            }
+        }
+    });
+}
+
+function milk() {
+    'use strict';
+    return new StateMachine({
+        init: 'refrigerator',
+        data: {
+            type: 'ingredient',
+            name: 'milk'
+        },
+        transitions: [
+            {name: 'pickUp', from: 'refrigerator', to: 'inventory'},
+            {name: 'addToPot', from: 'inventory', to: 'potContents'}
+        ],
+        methods: {
+            onPickUp: function () {
+                appendWindow('You take the milk from the refrigerator.');
+            },
+            onAddToPot: function () {
+                appendWindow('You add the milk to the pot.');
+            }
+        }
+    });
+}
+
+function cutHotDogs() {
+    'use strict';
+    return new StateMachine({
+        init: 'inventory',
+        data: {
+            type: 'ingredient',
+            name: 'cut up hot dogs'
+        },
+        transitions: [
+            {name: 'addToPot', from: 'inventory', to: 'potContents'}
+        ],
+        methods: {
+            onAddToPot: function () {
+                appendWindow('You add the cut up hotdogs to the pot.');
+            }
+        }
+    });
+}
+
+function hotDogs() {
+    'use strict';
+    return new StateMachine({
+        init: 'refrigerator',
+        data: {
+            type: 'ingredient',
+            name: 'hotdogs'
+        },
+        transitions: [
+            {name: 'pickUp', from: 'refrigerator', to: 'inventory'},
+            {name: 'addToPot', from: 'inventory', to: 'potContents'},
+            {name: 'cutUp', from: 'inventory', to: 'cutUpHotDogs'}
+        ],
+        methods: {
+            onPickUp: function () {
+                appendWindow('You take the pack of hotdogs out of the refrigerator.');
+            },
+            onAddToPot: function () {
+                appendWindow('You empty the pack of hotdogs into the pot.');
+            },
+            onCutUp: function () {
+                initCutUpHotDogs();
+                delete game.hotDogs;
+            }
+        }
+    });
+}
+
+function noodles() {
+    'use strict';
+    return new StateMachine({
+        init: 'inventory',
+        data: {
+            type: 'ingredient',
+            name: 'elbow macaroni'
+        },
+        transitions: [
+            {name: 'addToPot', from: 'inventory', to: 'potContents'}
+        ],
+        methods: {
+            onAddToPot: function () {
+                appendWindow('You add the noodles to the pot.');
+            }
+        }
+    });
+}
+
+function cheesePacket() {
+    'use strict';
+    return new StateMachine({
+        init: 'inventory',
+        data: {
+            type: 'ingredient',
+            name: 'packet of cheese powder'
+        },
+        transitions: [
+            {name: 'open', from: 'inventory', to: 'openPacket'},
+            {name: 'addToPot', from: 'openPacket', to: 'potContents'}
+        ],
+        methods: {
+            onOpen: function () {
+                appendWindow('You tear open the cheese powder packet.');
+            },
+            onAddToPot: function () {
+                appendWindow('You add the cheese powder to the pot.');
+            }
+        }
+    });
+}
+
+function kraftDinner() {
+    'use strict';
+    return new StateMachine({
+        init: 'cabinet',
+        data: {
+            type: 'ingredient',
+            name: 'box of Kraft Dinner'
+        },
+        transitions: [
+            {name: 'pickUp', from: 'cabinet', to: 'inventory'},
+            {name: 'open', from: 'inventory', to: 'openKraftDinner'}
+        ],
+        methods: {
+            onPickUp: function () {
+                appendWindow('You take the box of Kraft Dinner out of the cabinet.');
+            },
+            onOpen: function () {
+                appendWindow('You open the box and find noodles and a cheese packet.');
+                initNoodlesAndCheese();
+                delete game.kraftDinner;
+            }
+        }
+    });
+}
+
--- main.js
+++ main.js
@@ -1,321 +1,31 @@
 var game = {};
 
-game.pot = new StateMachine({
-    init: 'cabinet',
-    data: {
-        type: 'vessel',
-        name: 'spaghetti pot'
-    },
-    transitions: [
-        {name: 'pickUp', from: ['cabinet', 'stove', 'sink'], to: 'inventory'},
-        {name: 'potOnStove', from: 'inventory', to: 'stove'},
-        {name: 'potInSink', from: 'inventory', to: 'sink'}
-    ],
-    methods: {
-        onPickUp: function () {
-            "use strict";
-            console.log('You pick up the pot.');
-        },
-        onPotOnStove: function () {
-            "use strict";
-            console.log('You put the pot on the stove.');
-        },
-        onPotInSink: function () {
-            "use strict";
-            console.log('You put the pot in the sink.');
-        }
-    }
-});
-
-game.colander = new StateMachine({
-    init: 'cabinet',
-    data: {
-        type: 'vessel',
-        name: 'colander'
-    },
-    transitions: [
-        {name: 'pickUp', from: ['cabinet', 'stove', 'sink'], to: 'inventory'},
-        {name: 'colanderOnStove', from: 'inventory', to: 'stove'},
-        {name: 'colanderInSink', from: 'inventory', to: 'sink'}
-    ],
-    methods: {
-        onPickUp: function () {
-            "use strict";
-            console.log('You pick up the colander.');
-        },
-        onColanderOnStove: function () {
-            "use strict";
-            console.log('You put the colander on the stove.');
-        },
-        onColanderInSink: function () {
-            "use strict";
-            console.log('You put the colander in the sink.');
-        }
-    }
-});
-
-function initLocations() {
-    "use strict";
-    game.sink = new StateMachine({
-        init: 'off',
-        data: {
-            type: 'location',
-            name: 'sink',
-            description: 'A typical steel apartment kitchen sink.'
-        },
-        transitions: [
-            {name: 'step', from: 'off', to: 'on'},
-            {name: 'step', from: 'on', to: 'off'}
-        ],
-        methods: {
-            onOn: function () {
-                console.log('You turn on the sink. The water is running.');
-            },
-            onOff: function () {
-                console.log('You turn off the stove. The water stops running.');
-            }
-        }
-    });
-
-    game.stove = new StateMachine({
-        init: 'off',
-        data: {
-            type: 'location',
-            name: 'stove',
-            description: 'A black electric stove. You should probably clean it.'
-        },
-        transitions: [
-            {name: 'step', from: 'off', to: 'on'},
-            {name: 'step', from: 'on', to: 'off'}
-        ],
-        methods: {
-            onOn: function () {
-                console.log('You turn on the stove. The element starts to glow.');
-            },
-            onOff: function () {
-                console.log('You turn off the stove. The element stops glowing.');
-            }
-        }
-    });
-
-    game.cabinet = new StateMachine({
-        init: 'closed',
-        data: {
-            type: 'location',
-            name: 'cabinet',
-            description: 'This kitchen does have a lot of cabinets, but you have a sneaking suspicion they\'re not attached to the wall.'
-        },
-        transitions: [
-            {name: 'step', from: 'closed', to: 'open'},
-            {name: 'step', from: 'open', to: 'closed'}
-        ],
-        methods: {
-            onOpen: function () {
-                console.log('You opened the cabinet.');
-            },
-            onClose: function () {
-                console.log('You closed the cabinet.');
-            }
-        }
-    });
-
-    game.fridge = new StateMachine({
-        init: 'closed',
-        data: {
-            type: 'location',
-            name: 'fridge',
-            description: 'The top-freezer fridge that came with the apartment. Sometimes, if freezes your milk for no reason.'
-        },
-        transitions: [
-            {name: 'step', from: 'closed', to: 'open'},
-            {name: 'step', from: 'open', to: 'closed'}
-        ],
-        methods: {
-            onOpen: function () {
-                console.log('You opened the fridge.');
-            },
-            onClose: function () {
-                console.log('You closed the fridge.');
-            }
-        }
-    });
-
-    game.freezer = new StateMachine({
-        init: 'closed',
-        data: {
-            type: 'location',
-            name: 'freezer',
-            description: 'The freezer. Mostly frozen veggies and geriatric bananas.'
-        },
-        transitions: [
-            {name: 'step', from: 'closed', to: 'open'},
-            {name: 'step', from: 'open', to: 'closed'}
-        ],
-        methods: {
-            onOpen: function () {
-                console.log('You opened the freezer.');
-            },
-            onClose: function () {
-                console.log('You closed the freezer.');
-            }
-        }
-    });
-}
-
-game.carrotsAndPeas = new StateMachine({
-    init: 'freezer',
-    data: {
-        type: 'ingredient',
-        name: 'carrots and peas'
-    },
-    transitions: [
-        {name: 'pickUp', from: 'freezer', to: 'inventory'},
-        {name: 'addToPot', from: 'inventory', to: 'potContents'}
-    ],
-    methods: {
-        onPickUp: function () {
-            "use strict";
-            console.log('You take the bag from the freezer.');
-        },
-        onAddToPot: function () {
-            "use strict";
-            console.log('You empty the bag of peas and carrots into the pot.');
-        }
-    }
-});
-
-game.butter = new StateMachine({
-    init: 'refrigerator',
-    data: {
-        type: 'ingredient',
-        name: 'butter'
-    },
-    transitions: [
-        {name: 'pickUp', from: 'freezer', to: 'inventory'},
-        {name: 'addToPot', from: 'inventory', to: 'potContents'}
-    ],
-    methods: {
-        onPickUp: function () {
-            "use strict";
-            console.log('You take the butter from the refrigerator.');
-        },
-        onAddToPot: function () {
-            "use strict";
-            console.log('You add the butter to the pot.');
-        }
-    }
-});
-
-function initCutUpHotDogs() {
-    "use strict";
-    game.cutHotDogs = new StateMachine({
-        init: 'inventory',
-        data: {
-            type: 'ingredient',
-            name: 'cut up hot dogs'
-        },
-        transitions: [
-            {name: 'addToPot', from: 'inventory', to: 'potContents'}
-        ],
-        methods: {
-            onAddToPot: function () {
-                console.log('You add the cut up hotdogs to the pot.');
-            }
-        }
-    });
-}
-
-game.hotDogs = new StateMachine({
-    init: 'refrigerator',
-    data: {
-        type: 'ingredient',
-        name: 'hotdogs'
-    },
-    transitions: [
-        {name: 'pickUp', from: 'refrigerator', to: 'inventory'},
-        {name: 'addToPot', from: 'inventory', to: 'potContents'},
-        {name: 'cutUp', from: 'inventory', to: 'cutUpHotDogs'}
-    ],
-    methods: {
-        onPickUp: function () {
-            "use strict";
-            console.log('You take the pack of hotdogs out of the refrigerator.');
-        },
-        onAddToPot: function () {
-            "use strict";
-            console.log('You empty the pack of hotdogs into the pot.');
-        },
-        onCutUp: function () {
-            "use strict";
-            initCutUpHotDogs();
-            delete game.hotDogs;
-        }
-    }
-});
+game.home = home();
+game.kitchen = kitchen();
+game.pot = pot();
+game.colander = colander();
+game.carrotsAndPeas = carrotsAndPeas();
+game.butter = butter();
+game.milk = milk();
+game.hotDogs = hotDogs();
+game.kraftDinner = kraftDinner();
 
 function initNoodlesAndCheese() {
-    "use strict";
-    game.noodles = new StateMachine({
-        init: 'inventory',
-        data: {
-            type: 'ingredient',
-            name: 'elbow macaroni'
-        },
-        transitions: [
-            {name: 'addToPot', from: 'inventory', to: 'potContents'}
-        ],
-        methods: {
-            onAddToPot: function () {
-                console.log('You add the noodles to the pot.');
-            }
-        }
-    });
-
-    game.cheesePacket = new StateMachine({
-        init: 'inventory',
-        data: {
-            type: 'ingredient',
-            name: 'packet of cheese powder'
-        },
-        transitions: [
-            {name: 'open', from: 'inventory', to: 'openPacket'},
-            {name: 'addToPot', from: 'openPacket', to: 'potContents'}
-        ],
-        methods: {
-            onOpen: function () {
-                console.log('You tear open the cheese powder packet.');
-            },
-            onAddToPot: function () {
-                console.log('You add the cheese powder to the pot.');
-            }
-        }
-    });
+    game.noodles = noodles();
+    game.cheesePacket = cheesePacket();
 }
 
-game.kraftDinner = new StateMachine({
-    init: 'cabinet',
-    data: {
-        type: 'ingredient',
-        name: 'box of Kraft Dinner'
-    },
-    transitions: [
-        {name: 'pickUp', from: 'cabinet', to: 'inventory'},
-        {name: 'open', from: 'inventory', to: 'openKraftDinner'}
-    ],
-    methods: {
-        onPickUp: function () {
-            "use strict";
-            console.log('You take the box of Kraft Dinner out of the cabinet.');
-        },
-        onOpen: function () {
-            "use strict";
-            console.log('You open the box and find noodles and a cheese packet.');
-            initNoodlesAndCheese();
-            delete game.kraftDinner;
-        }
-    }
-});
+function initCutUpHotDogs() {
+    game.cutUpHotDogs = cutHotDogs();
+}
 
+function initLocations() {
+    game.sink = sink();
+    game.stove = stove();
+    game.cabinet = cabinet();
+    game.fridge = fridge();
+    game.freezer = freezer();
+}
 
 function listByType(thetype) {
     "use strict";
@@ -329,10 +39,12 @@ function listByType(thetype) {
 }
 
 function describe(prop) {
-    appendToWindow(game[prop].description);
+    'use strict';
+    var str = game[prop].description;
+    appendWindow(str);
 }
 
-function appendToWindow(str) {
-    'use strict';
+function appendWindow(str) {
     $('.js-content').append('<p>' + str + '</p>');
 }
+
